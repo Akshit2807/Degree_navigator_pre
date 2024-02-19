57324795cdf8c5d097229be6dad26e76bf14ac15
Akshit Pahade <akpahade55@gmail.com>
2024-02-19 13:50:47 +0530

    Initial commit

---
+++ lib/screens/base_screen.dart
@@ -0,0 +1,32 @@
+import 'package:untitled2/constants/color.dart';
+import 'package:untitled2/constants/icons.dart';
+import 'package:untitled2/constants/size.dart';
+import 'package:untitled2/screens/featuerd_screen.dart';
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({Key? key}) : super(key: key);
+
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    FeaturedScreen(),
+    FeaturedScreen(),
+    FeaturedScreen(),
+    FeaturedScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+
+    );
+  }
+}
